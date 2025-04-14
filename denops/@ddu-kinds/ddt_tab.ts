4b460bdf25e49135565eadf05de3f75cadef0c89
Shougo Matsushita <Shougo.Matsu@gmail.com>
2025-04-14 09:34:29 +0900

    Update actions

--- denops/@ddu-kinds/ddt_tab.ts
+++ denops/@ddu-kinds/ddt_tab.ts
@@ -1,5 +1,5 @@
 import {
-  type ActionArguments,
+  type Actions,
   ActionFlags,
   type DduItem,
 } from "jsr:@shougo/ddu-vim@~10.3.0/types";
@@ -18,81 +18,84 @@ export type ActionData = {
 type Params = Record<string, never>;
 
 export class Kind extends BaseKind<Params> {
-  override actions: Record<
-    string,
-    (args: ActionArguments<Params>) => Promise<ActionFlags>
-  > = {
-    delete: async (args: { denops: Denops; items: DduItem[] }) => {
-      const currentTab = await fn.tabpagenr(args.denops);
-      const tabNrs = args.items
+  override actions: Actions<Params> = {
+    delete: {
+      description: "Delete ddt tab.",
+      callback: async (args: { denops: Denops; items: DduItem[] }) => {
+        const currentTab = await fn.tabpagenr(args.denops);
+        const tabNrs = args.items
         .map((item) => (item?.action as ActionData)?.tabNr)
         .filter((tabNr) => tabNr !== undefined && tabNr != currentTab);
 
-      for (const tabNr of tabNrs.sort().reverse()) {
-        await args.denops.cmd(`silent! tabclose ${tabNr}`);
-      }
+        for (const tabNr of tabNrs.sort().reverse()) {
+          await args.denops.cmd(`silent! tabclose ${tabNr}`);
+        }
 
-      return Promise.resolve(ActionFlags.Persist | ActionFlags.RefreshItems);
+        return Promise.resolve(ActionFlags.Persist | ActionFlags.RefreshItems);
+      },
     },
-    edit: async (args: { denops: Denops; items: DduItem[] }) => {
-      for (const item of args.items) {
-        const action = item?.action as ActionData;
-        const cwd = await fn.gettabvar(
-          args.denops,
-          action.tabNr,
-          "ddt_ui_last_directory",
-          await fn.getcwd(args.denops, 0, action.tabNr),
-        ) as string;
-
-        const newCwd = await fn.input(
-          args.denops,
-          "New ddt cwd: ",
-          cwd,
-          "dir",
-        );
-        await args.denops.cmd("redraw");
-        if (newCwd.length === 0 || newCwd === cwd) {
-          continue;
-        }
-
-        const stat = await safeStat(newCwd);
-
-        if (stat?.isFile) {
-          await printError(
+    edit: {
+      description: "Change ddt tab directory.",
+      callback: async (args: { denops: Denops; items: DduItem[] }) => {
+        for (const item of args.items) {
+          const action = item?.action as ActionData;
+          const cwd = await fn.gettabvar(
             args.denops,
-            `${newCwd} is not directory.`,
-          );
-          continue;
-        }
+            action.tabNr,
+            "ddt_ui_last_directory",
+            await fn.getcwd(args.denops, 0, action.tabNr),
+          ) as string;
 
-        if (!stat) {
-          const result = await fn.confirm(
+          const newCwd = await fn.input(
             args.denops,
-            `${newCwd} is not directory.  Create?`,
-            "&Yes\n&No\n&Cancel",
+            "New ddt cwd: ",
+            cwd,
+            "dir",
           );
-          if (result != 1) {
+          await args.denops.cmd("redraw");
+          if (newCwd.length === 0 || newCwd === cwd) {
             continue;
           }
 
-          await fn.mkdir(args.denops, newCwd, "p");
+          const stat = await safeStat(newCwd);
+
+          if (stat?.isFile) {
+            await printError(
+              args.denops,
+              `${newCwd} is not directory.`,
+            );
+            continue;
+          }
+
+          if (!stat) {
+            const result = await fn.confirm(
+              args.denops,
+              `${newCwd} is not directory.  Create?`,
+              "&Yes\n&No\n&Cancel",
+            );
+            if (result != 1) {
+              continue;
+            }
+
+            await fn.mkdir(args.denops, newCwd, "p");
+          }
+
+          await args.denops.cmd(`tabnext ${action.tabNr}`);
+
+          // Move to ddt buffer
+          const bufNr = await vars.t.get(args.denops, "ddt_ui_last_bufnr", -1);
+          if (bufNr > 0) {
+            await args.denops.cmd(`buffer ${bufNr}`);
+            await args.denops.call("ddt#ui#do_action", "cd", {
+              directory: newCwd,
+            });
+          }
+
+          await args.denops.cmd(`noautocmd tcd ${newCwd}`);
         }
 
-        await args.denops.cmd(`tabnext ${action.tabNr}`);
-
-        // Move to ddt buffer
-        const bufNr = await vars.t.get(args.denops, "ddt_ui_last_bufnr", -1);
-        if (bufNr > 0) {
-          await args.denops.cmd(`buffer ${bufNr}`);
-          await args.denops.call("ddt#ui#do_action", "cd", {
-            directory: newCwd,
-          });
-        }
-
-        await args.denops.cmd(`noautocmd tcd ${newCwd}`);
-      }
-
-      return Promise.resolve(ActionFlags.None);
+        return Promise.resolve(ActionFlags.None);
+      },
     },
     switch: async (args: { denops: Denops; items: DduItem[] }) => {
       for (const item of args.items) {
@@ -117,54 +120,57 @@ export class Kind extends BaseKind<Params> {
 
       return Promise.resolve(ActionFlags.None);
     },
-    new: async (args: { denops: Denops; items: DduItem[] }) => {
-      for (const item of args.items) {
-        const action = item?.action as ActionData;
-        const cwd = action.cwd.length === 0
-          ? await fn.getcwd(args.denops)
-          : action.cwd;
+    new: {
+      description: "Create new ddt tab directory.",
+      callback: async (args: { denops: Denops; items: DduItem[] }) => {
+        for (const item of args.items) {
+          const action = item?.action as ActionData;
+          const cwd = action.cwd.length === 0
+            ? await fn.getcwd(args.denops)
+            : action.cwd;
 
-        const newCwd = await fn.input(
-          args.denops,
-          "New cwd: ",
-          cwd,
-          "dir",
-        );
-        await args.denops.cmd("redraw");
-        if (newCwd.length === 0) {
-          continue;
-        }
-
-        // Note: Deno.stat() may be failed
-        try {
-          const fileInfo = await Deno.stat(newCwd);
-
-          if (fileInfo.isFile) {
-            await printError(
-              args.denops,
-              `${newCwd} is not directory.`,
-            );
-            continue;
-          }
-        } catch (_e: unknown) {
-          const result = await fn.confirm(
+          const newCwd = await fn.input(
             args.denops,
-            `${newCwd} is not directory.  Create?`,
-            "&Yes\n&No\n&Cancel",
+            "New cwd: ",
+            cwd,
+            "dir",
           );
-          if (result != 1) {
+          await args.denops.cmd("redraw");
+          if (newCwd.length === 0) {
             continue;
           }
 
-          await fn.mkdir(args.denops, newCwd, "p");
+          // Note: Deno.stat() may be failed
+          try {
+            const fileInfo = await Deno.stat(newCwd);
+
+            if (fileInfo.isFile) {
+              await printError(
+                args.denops,
+                `${newCwd} is not directory.`,
+              );
+              continue;
+            }
+          } catch (_e: unknown) {
+            const result = await fn.confirm(
+              args.denops,
+              `${newCwd} is not directory.  Create?`,
+              "&Yes\n&No\n&Cancel",
+            );
+            if (result != 1) {
+              continue;
+            }
+
+            await fn.mkdir(args.denops, newCwd, "p");
+          }
+
+          await args.denops.cmd(
+            `tabnext ${action.tabNr} | tabnew | tcd ${newCwd}`,
+          );
         }
 
-        await args.denops.cmd(
-          `tabnext ${action.tabNr} | tabnew | tcd ${newCwd}`,
-        );
-      }
-
-      return Promise.resolve(ActionFlags.None);
+        return Promise.resolve(ActionFlags.None);
+      },
     },
   };
 
